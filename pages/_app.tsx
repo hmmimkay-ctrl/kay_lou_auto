9a7b55ed1dedfcbd93144ed5333b45dd1d541d58
hmmimkay-ctrl <hmmimkay@gmail.com>
2026-01-09 19:30:00 +0700

    debug: Add console.log to verify _app.tsx is loaded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app';
 import './globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+    console.log('✅ _app.tsx loaded - globals.css imported');
+  
   return (
     <div>
       <Component {...pageProps} />
